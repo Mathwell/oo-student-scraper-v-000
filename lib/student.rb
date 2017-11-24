eec9570eb8795943b93acf62e6dcecc45e8bd97b
Olena Ageyeva <eageeva@hotmail.com>
2017-11-24 18:43:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    self.add_student_attributes(student_hash)
+    add_student_attributes(student_hash)
     #@name=student_hash[:name]
     #@location=student_hash[:location]
     @@all<<self
